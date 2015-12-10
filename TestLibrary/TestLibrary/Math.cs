7b1f4be2bb842a07b315caccd0fade8b37e151ee
mrtkyz <mrtkyz@gmail.com>
2015-12-10 10:45:50 +0200

    testlibrary added

---
+++ TestLibrary/TestLibrary/Math.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TestLibrary
+{
+    public class Math
+    {
+        public double Sum(List<double> numberList)
+        {
+            double number = 0;
+            foreach (var item in numberList)
+            {
+                number += item;
+            }
+            return number;
+        }
+    }
+}
